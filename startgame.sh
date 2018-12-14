b19f1580c73757eab4044c99362cb11084972384
pycdr <44697997+pycdr@users.noreply.github.com>
2018-12-14 13:27:01 +0330

    Add files via upload

---
+++ startgame.sh
@@ -0,0 +1,23 @@
+cloneFromGit(){
+	echo '[*error*] "noisyrect" file not found!'
+	git clone https://github.com/pycdr/noisyrect
+	echo '[*git*] git cloned from https://github.com/pycdr/noisyrect'
+	mv ./noisyrect/* .
+	echo '[*start*]'
+	./noisyrect.pyc
+}
+compileSource(){
+	echo '[*error*] error when clone'
+	echo '[*py3compile*] compile "source.py" ...'
+	py3compile source.py
+	echo '[*fine*] compiled'
+	mv ./__pycache__/* ./__pycache__/noisyrect.pyc
+	mv ./__pycache__/* .
+	rm -r ./__pycache__
+	echo '[*start*]'
+	./noisyrect.pyc
+}
+echo '[*start*]'
+./noisyrect.pyc || compileSource() || cloneFromGit() || echo '[*error*] error when run game :('
+echo '[*end*] exit...'
+exit
